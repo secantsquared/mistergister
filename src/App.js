bcd10a4e03b72dd76904c31d5cbb5886881d22ab
Ryan Boris <ryan.boris@gmail.com>
2020-08-11 07:16:56 -0400

    :sparkles: Provide client the ability to load image files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import AbsImg from './assets/images/abs1.jpg'
 
 export default function App() {
   return (
     <div>
       <h1>Mister Gister! Yass!</h1>
+      <img src={AbsImg} alt="abstract" />
     </div>
   )
 }
