5bd67cf6332cff5c6d13a89aa97659549ab9cdd6
Ryan Boris <ryan.boris@gmail.com>
2020-08-11 07:02:12 -0400

    :wrench: Add webpack configuration

---
+++ webpack/webpack.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+
+const { merge } = require('webpack-merge')
+
+const commonConfig = require('./webpack.common.js')
+
+module.exports = ({ env }) => {
+  const envConfig = require(`./webpack.${env}.js`)
+  return merge(commonConfig, envConfig)
+}
